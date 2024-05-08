8c4e46f5443e2f72fce48c2a37c8acb34edfc0ca
TaQuangLinh <taquanglinh05bg@gmail.com>
2024-05-08 12:11:52 +0700

    Create HelloApplication.java

---
+++ BTL_DictionaryApp_Final/BTL_0405/BTL_0405/BTL - Da_co_game/Test13/BTL - TaQuangLinh/src/main/java/com/example/demo7/HelloApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo7;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+// file chính để chạy sản phẩm cuối.
+public class HelloApplication extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 911, 640);
+        stage.setTitle("Dictionary App");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
